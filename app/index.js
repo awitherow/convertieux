6b2b98425d2a82e466d7766522f898b82f200fb0
awitherow <au.witherow@gmail.com>
2017-09-06 11:39:32 +0200

    add basic api todo

--- app/index.js
+++ app/index.js
@@ -10,6 +10,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
+app.get('/api/conversions', function (req, res) {
+  // TODO: add coin conversion logic
+  // - query coinbase
+  // - for each coin get the coin/fiat price
+  // - calculate which number is needed to get
+  // - - conversion rate from coin -> fiat
+  // - - conversion rate from fiat -> coin
+  // res.status(200).json({ success: true, data }) -> success
+  // res.status(500).json({ status: 'error', success: false, err }) -> error
+})
+
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
